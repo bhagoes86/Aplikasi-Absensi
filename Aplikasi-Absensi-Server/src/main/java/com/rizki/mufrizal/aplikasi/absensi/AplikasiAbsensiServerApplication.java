6b8ad8623c10eb49d8129acdb3c7e0ded195c595
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-12 14:01:37 +0700

    initial server

---
+++ Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/AplikasiAbsensiServerApplication.java
@@ -0,0 +1,12 @@
+package com.rizki.mufrizal.aplikasi.absensi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiAbsensiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiAbsensiServerApplication.class, args);
+	}
+}
---
+++ Aplikasi-Absensi-Server/src/test/java/com/rizki/mufrizal/aplikasi/absensi/AplikasiAbsensiServerApplicationTests.java
@@ -0,0 +1,18 @@
+package com.rizki.mufrizal.aplikasi.absensi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AplikasiAbsensiServerApplication.class)
+@WebAppConfiguration
+public class AplikasiAbsensiServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
