53d03dbeb1f54a97ce14cb4629d50a92a04be6b3
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-12 19:30:34 +0700

    tambah konfigurasi spring security

---
+++ Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,51 @@
+package com.rizki.mufrizal.aplikasi.absensi.configuration;
+
+import com.rizki.mufrizal.aplikasi.absensi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Feb 12, 2016
+ * @Time 7:26:19 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Absensi-Server
+ * @Package com.rizki.mufrizal.aplikasi.absensi.configuration
+ *
+ */
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
