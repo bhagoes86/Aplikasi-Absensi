085c2e67f221674185dc2d1fe2d8a0ca87ca5a77
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-14 11:21:58 +0700

    tambah absensi hari repository

---
+++ Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/repository/AbsensiHariRepository.java
@@ -0,0 +1,29 @@
+package com.rizki.mufrizal.aplikasi.absensi.repository;
+
+import com.rizki.mufrizal.aplikasi.absensi.domain.AbsensiHari;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.Description;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Feb 14, 2016
+ * @Time 8:50:06 AM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Absensi-Server
+ * @Package com.rizki.mufrizal.aplikasi.absensi.repository
+ *
+ */
+@PreAuthorize("hasRole('ROLE_ADMIN')")
+@RepositoryRestResource(collectionResourceRel = "absensi", path = "absensi", collectionResourceDescription = @Description("API untuk crud data absensi"))
+public interface AbsensiHariRepository extends PagingAndSortingRepository<AbsensiHari, String> {
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @Override
+    public Page<AbsensiHari> findAll(Pageable pgbl);
+
+}
