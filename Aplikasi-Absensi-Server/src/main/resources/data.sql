bc07251eee81365e9d312030cc4a455393aa366e
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-14 19:45:28 +0700

    update data sql

--- Aplikasi-Absensi-Server/src/main/resources/data.sql
+++ Aplikasi-Absensi-Server/src/main/resources/data.sql
@@ -13,14 +13,8 @@ INSERT INTO tb_asisten(npm, enable, kelas, nama, password)
 VALUES 
 ('58412085', true, '4IA04', 'Rizki Mufrizal', '$2a$10$e3r6aos5Apwe21dtohD0pO/ikeqlM0KKzoBGY/KMr1z.vsJYLk/hC');
 
-INSERT INTO tb_absensi_hari(id_absensi_hari, hari)
-VALUES
-('1', 'senin'),
-('2', 'selasa'),
-('3', 'rabu'),
-('4', 'kamis'),
-('5', 'jumat'),
-('6', 'sabtu');
+INSERT INTO tb_asisten_role(id_asisten_role, role, npm)
+VALUES ('2', 'ROLE_USER', '58412085');
 
-INSERT INTO public.tb_absensi_kehadiran(id_absen_kehadiran, baris, kelas_praktikum, mata_praktikum, nama_asisten, nama_asisten_tetap, npm_asisten, ruang_praktikum, sesi, id_absensi_hari)
-VALUES ('1', 'A', 'RPL', 'rizki', 'mufrizal', '58412084', 'E1', '1', 1, '1');
+INSERT INTO public.tb_absensi_asisten(id_absen_asisten, baris, hari, kelas_praktikum, mata_praktikum, nama_asisten, nama_asisten_tetap, npm_asisten, ruang_praktikum, sesi, tanggal_jaga)
+VALUES ('1', 'A', 'senin', '4IA04', 'RPL', 'rizki', 'mufrizal', '58412085', 'E1', 1, '2016-02-14');
