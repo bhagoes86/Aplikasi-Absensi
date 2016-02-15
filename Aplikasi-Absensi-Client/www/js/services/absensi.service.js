b21c80720da6d7d7e55f5cde495c7c084bd38fc0
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-15 11:42:38 +0700

    tambah absensi service

---
+++ Aplikasi-Absensi-Client/www/js/services/absensi.service.js
@@ -0,0 +1,10 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Feb 14, 2016
+ * @Time 9:55:19 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Absensi-Client
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
+ * 
+ */
