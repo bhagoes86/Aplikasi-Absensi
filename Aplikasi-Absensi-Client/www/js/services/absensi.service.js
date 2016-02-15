e1de52c37592490a7d0d061ae876dda940b7e608
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-15 19:28:13 +0700

    update absensi service

--- Aplikasi-Absensi-Client/www/js/services/absensi.service.js
+++ Aplikasi-Absensi-Client/www/js/services/absensi.service.js
@@ -13,10 +13,14 @@ angular.module('Aplikasi-Absensi')
   .factory('AbsensiService', ['$http', function($http) {
 
     var baseUrl = 'http://192.168.2.116:8080';
+    //var baseUrl = 'http://127.0.0.1:8080';
 
     return {
       simpanAbsensiAsisten: function(absensi) {
-        return $http.post(baseUrl + '/api/absensi');
+        return $http.post(baseUrl + '/api/absensi', absensi);
+      },
+      checkNpmAsisten: function(npm) {
+        return $http.get(baseUrl + '/api/asisten/' + npm);
       }
     };
 
