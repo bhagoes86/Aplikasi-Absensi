49c5034d0713aea4ffced5a728aaa16d142ea544
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-15 18:11:06 +0700

    update untuk bagian scan

--- Aplikasi-Absensi-Client/www/js/controllers/absensi.controller.js
+++ Aplikasi-Absensi-Client/www/js/controllers/absensi.controller.js
@@ -10,15 +10,58 @@
  */
 
 angular.module('Aplikasi-Absensi')
-  .controller('AbsensiController', ['$scope', '$cordovaBarcodeScanner', function($scope, $cordovaBarcodeScanner) {
+  .controller('AbsensiController', ['$scope', '$cordovaBarcodeScanner', 'AbsensiService', '$ionicModal', '$ionicPopup', function($scope, $cordovaBarcodeScanner, AbsensiService, $ionicModal, $ionicPopup) {
 
     $scope.hasilScan = '';
+    $scope.modalInputDataAsisten = {};
+    $scope.inputAbsensiAsisten = {};
+    $scope.dataAbsensiAsisten = {};
+
+    function inputDataAsisten() {
+      $ionicModal.fromTemplateUrl('templates/inputDataAsisten.html', {
+        scope: $scope
+      }).then(function(modal) {
+        $scope.modalInputDataAsisten = modal;
+        $scope.modalInputDataAsisten.show();
+      });
+    }
+
+    inputDataAsisten();
+
+    $scope.simpanAbsensiAsisten = function(a) {
+      var d = new Date();
+      var hari = ["minggu", "senin", "selasa", "rabu", "kamis", "jumat", "sabtu"];
+
+      $scope.dataAbsensiAsisten.namaAsistenTetap = a.namaAsistenTetap;
+      $scope.dataAbsensiAsisten.barisJaga = a.barisJaga;
+      $scope.dataAbsensiAsisten.mataPraktikum = a.mataPraktikum;
+      $scope.kelasPraktikum = a.kelasPraktikum;
+      $scope.ruangPraktikum = a.ruangPraktikum;
+      $scope.dataAbsensiAsisten.sesiJaga = a.sesiJaga;
+      $scope.dataAbsensiAsisten.hariJaga = hari[d.getDay()];
+      $scope.dataAbsensiAsisten.tanggalJaga = d;
+
+      $scope.modalInputDataAsisten.hide();
+    };
 
     $scope.scanBarcode = function() {
       $cordovaBarcodeScanner
         .scan()
         .then(function(barcodeData) {
-          $scope.hasilScan = barcodeData.text;
+          $scope.dataAbsensiAsisten.npmAsisten = barcodeData.text;
+
+          AbsensiService.simpanAbsensiAsisten($scope, dataAbsensiAsisten).success(function(data) {
+            $ionicPopup.show({
+              template: 'Berhasil melakukan absensi',
+              title: 'Info',
+              scope: $scope,
+              buttons: [{
+                text: '<b>OK</b>',
+                type: 'button-positive'
+              }]
+            });
+          });
+
         }, function(error) {});
     };
 
