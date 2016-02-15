4cffcfb04b863267d72b6d384005bad94397df42
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-15 12:33:51 +0700

    tambah konfigurasi jaspert report

--- Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/configuration/WebConfiguration.java
+++ Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/configuration/WebConfiguration.java
@@ -1,8 +1,11 @@
 package com.rizki.mufrizal.aplikasi.absensi.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsMultiFormatView;
+import org.springframework.web.servlet.view.jasperreports.JasperReportsViewResolver;
 
 /**
  *
@@ -20,5 +23,16 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+    }
+
+    @Bean
+    public JasperReportsViewResolver jasperReportsViewResolver() {
+        JasperReportsViewResolver reportsViewResolver = new JasperReportsViewResolver();
+        reportsViewResolver.setPrefix("classpath:/reports/");
+        reportsViewResolver.setSuffix(".jrxml");
+        reportsViewResolver.setViewNames("report_*");
+        reportsViewResolver.setViewClass(JasperReportsMultiFormatView.class);
+        reportsViewResolver.setOrder(0);
+        return reportsViewResolver;
     }
 }
