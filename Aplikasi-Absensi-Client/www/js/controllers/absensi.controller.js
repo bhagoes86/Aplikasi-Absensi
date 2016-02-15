91ccac3ee2397f50afd23c97d49c6099b92d3626
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-15 12:11:16 +0700

    update absensi controller

--- Aplikasi-Absensi-Client/www/js/controllers/absensi.controller.js
+++ Aplikasi-Absensi-Client/www/js/controllers/absensi.controller.js
@@ -6,5 +6,20 @@
  * @Encoding UTF-8
  * @Project Aplikasi-Absensi-Client
  * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
- * 
+ *
  */
+
+angular.module('Aplikasi-Absensi')
+  .controller('AbsensiController', ['$scope', '$cordovaBarcodeScanner', function($scope, $cordovaBarcodeScanner) {
+
+    $scope.hasilScan = '';
+
+    $scope.scanBarcode = function() {
+      $cordovaBarcodeScanner
+        .scan()
+        .then(function(barcodeData) {
+          $scope.hasilScan = barcodeData.text;
+        }, function(error) {});
+    };
+
+  }]);
