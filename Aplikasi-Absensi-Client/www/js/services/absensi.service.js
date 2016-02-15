7ec4651a0d951a41beff8cde4eb2631bde7e8cd8
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-15 18:11:15 +0700

    tambah absensi service

--- Aplikasi-Absensi-Client/www/js/services/absensi.service.js
+++ Aplikasi-Absensi-Client/www/js/services/absensi.service.js
@@ -6,5 +6,18 @@
  * @Encoding UTF-8
  * @Project Aplikasi-Absensi-Client
  * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
- * 
+ *
  */
+
+angular.module('Aplikasi-Absensi')
+  .factory('AbsensiService', ['$http', function($http) {
+
+    var baseUrl = 'http://192.168.2.116:8080';
+
+    return {
+      simpanAbsensiAsisten: function(absensi) {
+        return $http.post(baseUrl + '/api/absensi');
+      }
+    };
+
+  }]);
