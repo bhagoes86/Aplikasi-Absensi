3626092da839ccad5c193d6a335b22a8d5c270b1
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-17 22:45:46 +0700

    tambah report absensi berdasarkan tanggal

--- Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/controller/ReportController.java
+++ Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/controller/ReportController.java
@@ -3,7 +3,11 @@ package com.rizki.mufrizal.aplikasi.absensi.controller;
 import com.rizki.mufrizal.aplikasi.absensi.domain.Asisten;
 import com.rizki.mufrizal.aplikasi.absensi.repository.AbsensiAsistenRepository;
 import com.rizki.mufrizal.aplikasi.absensi.repository.AsistenRepository;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -53,6 +57,27 @@ public class ReportController {
             ModelAndView modelAndView) {
 
         modelAndView.addObject("dataSource", absensiAsistenRepository.findAll());
+        modelAndView.addObject("format", "pdf");
+
+        modelAndView.setViewName("report_absensi_asisten");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/LaporanAbsensiAsistenPerTanggal", method = RequestMethod.GET)
+    public ModelAndView generateLaporanAbsensiAsistenBerdasarkanTanggal(
+            ModelAndView modelAndView,
+            @RequestParam(value = "tanggalAwal") String tanggalAwal,
+            @RequestParam(value = "tanggalAkhir") String tanggalAkhir) throws ParseException {
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        Date dateAwal = simpleDateFormat.parse(tanggalAwal);
+        Date dateAKhir = simpleDateFormat.parse(tanggalAkhir);
+
+        LocalDate localDateAwal = LocalDate.parse(new SimpleDateFormat("yyyy-MM-dd").format(dateAwal));
+        LocalDate localDateAkhir = LocalDate.parse(new SimpleDateFormat("yyyy-MM-dd").format(dateAKhir));
+
+        modelAndView.addObject("dataSource", absensiAsistenRepository.findByTanggalJagaBetween(localDateAwal, localDateAkhir));
         modelAndView.addObject("format", "pdf");
 
         modelAndView.setViewName("report_absensi_asisten");
