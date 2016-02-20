ce878a6cdd73f87ad424906ff207d929e22b18c9
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-20 21:51:32 +0700

    tambah query find one

--- Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/repository/AbsensiAsistenRepository.java
+++ Aplikasi-Absensi-Server/src/main/java/com/rizki/mufrizal/aplikasi/absensi/repository/AbsensiAsistenRepository.java
@@ -5,8 +5,10 @@ import java.time.LocalDate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.Description;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 /**
@@ -25,7 +27,11 @@ public interface AbsensiAsistenRepository extends PagingAndSortingRepository<Abs
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @Override
-    public Page<AbsensiAsisten> findAll(Pageable pgbl);
+    public Page<AbsensiAsisten> findAll(Pageable pageable);
+
+    @PreAuthorize("permitAll")
+    @RestResource(path = "asisten", rel = "asisten")
+    public Page<AbsensiAsisten> findByNpmAsisten(@Param("npmAsisten") String npmAsisten, Pageable pageable);
 
     @PreAuthorize("permitAll")
     @Override
