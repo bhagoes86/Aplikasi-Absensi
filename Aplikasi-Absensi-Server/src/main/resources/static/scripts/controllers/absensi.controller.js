40bd62a5bce4e4903fd2e953383f827eb0c5eded
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-20 21:47:08 +0700

    update absensi controller untuk search

--- Aplikasi-Absensi-Server/src/main/resources/static/scripts/controllers/absensi.controller.js
+++ Aplikasi-Absensi-Server/src/main/resources/static/scripts/controllers/absensi.controller.js
@@ -14,9 +14,9 @@
   angular.module('Aplikasi-Absensi')
     .controller('AbsensiController', AbsensiController);
 
-  AbsensiController.$inject = ['AbsensiService'];
+  AbsensiController.$inject = ['AbsensiService', 'AuthService'];
 
-  function AbsensiController(AbsensiService) {
+  function AbsensiController(AbsensiService, AuthService) {
     var absensi = this;
 
     absensi.paging = {
@@ -24,9 +24,12 @@
       jumlah: 5
     };
 
+    absensi.enableSearch = false;
+
     absensi.dataAbsensi = {};
 
     function getAllAbsensi() {
+      absensi.enableSearch = false;
       AbsensiService.getAllAbsensiAsisten(absensi.paging.page, absensi.paging.jumlah).success(function(data) {
         absensi.dataAbsensi = data._embedded.absensi;
         absensi.paging.totalPages = data.page.totalPages;
@@ -35,33 +38,93 @@
 
     getAllAbsensi();
 
+    absensi.getAllAbsensi = function() {
+      absensi.enableSearch = false;
+      absensi.paging.page = 0;
+      absensi.paging.jumlah = 5;
+      getAllAbsensi();
+    };
+
+    //paging all
+
     absensi.nextPage = function() {
-      if (absensi.paging.page < absensi.paging.totalPages) {
+      if (absensi.paging.page + 1 < absensi.paging.totalPages) {
         absensi.paging.page++;
-        getAllAbsensi()();
+        getAllAbsensi();
       }
     };
 
     absensi.hasNextPage = function() {
-      if (absensi.paging.page < absensi.paging.totalPages) {
-        return false;
+      if (absensi.paging.page + 1 < absensi.paging.totalPages) {
+        return true;
       }
-      return true;
+      return false;
     };
 
     absensi.previousPage = function() {
-      if (absensi.paging.page > 1) {
+      if (absensi.paging.page > 0) {
         absensi.paging.page--;
-        getAllAbsensi()();
+        getAllAbsensi();
       }
     };
 
     absensi.hasPreviousPage = function() {
-      if (absensi.paging.page > 1) {
-        return false;
+      if (absensi.paging.page > 0) {
+        return true;
       }
-      return true;
+      return false;
     };
+
+    //end paging all
+
+    absensi.npm = '';
+
+    absensi.searchAbsensiAsistenByNpm = function(npm) {
+      absensi.enableSearch = true;
+      absensi.npm = npm;
+      absensi.paging.page = 0;
+      absensi.paging.jumlah = 5;
+      AbsensiService.searchAbsensiAsistenByNpm(npm, absensi.paging.page, absensi.paging.jumlah).success(function(data) {
+        if (angular.equals([], data._embedded.absensi)) {
+          swal('Warning', 'maaf, data tidak ditemukan', 'error');
+        } else {
+          absensi.dataAbsensi = data._embedded.absensi;
+          absensi.paging.totalPages = data.page.totalPages;
+        }
+      });
+    };
+
+    //paging search
+
+    absensi.nextPageSearch = function() {
+      if (absensi.paging.page + 1 < absensi.paging.totalPages) {
+        absensi.paging.page++;
+        absensi.searchAbsensiAsistenByNpm(absensi.npm);
+      }
+    };
+
+    absensi.hasNextPageSearch = function() {
+      if (absensi.paging.page + 1 < absensi.paging.totalPages) {
+        return true;
+      }
+      return false;
+    };
+
+    absensi.previousPageSearch = function() {
+      if (absensi.paging.page > 0) {
+        absensi.paging.page--;
+        absensi.searchAbsensiAsistenByNpm(absensi.npm);
+      }
+    };
+
+    absensi.hasPreviousPageSearch = function() {
+      if (absensi.paging.page > 0) {
+        return true;
+      }
+      return false;
+    };
+
+    //end paging search
 
     absensi.cetakLaporan = function() {
       $('#modalLaporan').openModal();
@@ -75,6 +138,10 @@
       } else {
         window.open('/LaporanAbsensiAsistenPerTanggal?tanggalAwal=' + tanggalAwal + '&tanggalAkhir=' + tanggalAkhir, '_blank');
       }
+    };
+
+    absensi.logout = function() {
+      AuthService.logout();
     };
 
   }
