f29e63ea8d0b0071c9bd284d81c3d84db204fe9f
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-20 21:47:25 +0700

    tambah home controller

---
+++ Aplikasi-Absensi-Server/src/main/resources/static/scripts/controllers/home.controller.js
@@ -0,0 +1,39 @@
+/**
+ *
+ * @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
+ * @Since Feb 20, 2016
+ * @Time 7:39:58 PM
+ * @Encoding UTF-8
+ * @Project Aplikasi-Absensi-Server
+ * @Package Expression package is undefined on line 8, column 15 in Templates/Other/javascript.js.
+ *
+ */
+
+(function() {
+  'use strict';
+  angular.module('Aplikasi-Absensi')
+    .controller('HomeController', HomeController);
+
+  HomeController.$inject = ['AsistenService'];
+
+  function HomeController(AsistenService) {
+    var home = this;
+
+    home.cetakKartu = function() {
+      $('#modalKartu').openModal();
+    };
+
+    home.checkAsisten = function(npm) {
+      AsistenService.getAsisten(npm).success(function(data) {
+        window.open('/KartuAsisten?npm=' + npm, '_blank');
+        $('#modalKartu').closeModal();
+      }).error(function(data, status) {
+        if (status === 404) {
+          swal('Warning', 'anda belum melakukan registrasi, silahkan registrasi untuk mencetak kartu asisten', 'error');
+        }
+      });
+    };
+
+  }
+
+})();
