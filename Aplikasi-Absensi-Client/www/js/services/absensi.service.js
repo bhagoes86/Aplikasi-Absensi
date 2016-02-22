685cacf8b0c639cadc86348e7394c1c77a2a5fb4
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-22 20:21:51 +0700

    update url

--- Aplikasi-Absensi-Client/www/js/services/absensi.service.js
+++ Aplikasi-Absensi-Client/www/js/services/absensi.service.js
@@ -12,7 +12,8 @@
 angular.module('Aplikasi-Absensi')
   .factory('AbsensiService', ['$http', function($http) {
 
-    var baseUrl = 'http://192.168.2.116:8080';
+    //var baseUrl = 'http://192.168.2.116:8080';
+    var baseUrl = 'https://absensi-asisten.rhcloud.com:443';
 
     return {
       simpanAbsensiAsisten: function(absensi) {
